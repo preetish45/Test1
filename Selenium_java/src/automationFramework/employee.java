4a154f8c00845293ea69209fd6be3f4bf2b71081
PB <PB@PB-PC>
2018-05-15 06:08:04 +0530

    test

--- Selenium_java/src/automationFramework/employee.java
+++ Selenium_java/src/automationFramework/employee.java
@@ -10,22 +10,19 @@ public class employee{
 
 public static WebDriver driver;
 
-public static void main(String[] args) {
- System.setProperty("webdriver.gecko.driver","D:/Driver/BrowserDriver/geckodriver-v0.20.0-win32/geckodriver.exe");
+public static void main(String[] args) throws InterruptedException {
+ System.setProperty("webdriver.gecko.driver","D:/Driver/BrowserDriver/geckodriver-v0.16.0-win32/geckodriver.exe");
 
  driver = new FirefoxDriver();
 
  driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
- driver.get("http://www.onlinestore.toolsqa.wpengine.com");
+ driver.get("https://www.paypal.com/in/home");
 
- WebElement element = driver.findElement(By.linkText("Product Category"));
-
- Actions action = new Actions(driver);
-
- action.moveToElement(element).build().perform();
-
- driver.findElement(By.linkText("iPads")).click();
-
+ WebElement signin = driver.findElement(By.xpath("//a[@id='signup-button']"));
+ signin.click();
+ Thread.sleep(30);
+WebElement next =driver.findElement(By.xpath("//*[@id='get-started-carousel']/div/div[2]/a"));
+next.click();
  }
 }
